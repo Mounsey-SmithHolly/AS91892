dc124706cc99bd1235467bf7f4b77bfee89be93a
Mounsey-SmithHolly <mounseysmithho@wgc.school.nz>
2025-09-02 23:19:11 +0000

    changes

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ def render_animals():
     return render_template('animals.html', animals=animal_list)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
