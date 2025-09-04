806ae5dba4d97ed5eee45522859bd78b4ef6f6bc
Mounsey-SmithHolly <mounseysmithho@wgc.school.nz>
2025-09-04 05:58:32 +0000

    finishing the search and adding the get_animals and get_classifications

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ def render_search():
     return render_template('cretures.html', animals=animal_list)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
 
