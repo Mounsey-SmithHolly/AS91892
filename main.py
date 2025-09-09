2f0a8e7fbd4f5bc0b22957cf70f8413d89bb1ddf
Mounsey-SmithHolly <mounseysmithho@wgc.school.nz>
2025-09-09 09:05:40 +0000

    beguining to add code for the filters

--- main.py
+++ main.py
@@ -13,9 +13,26 @@ def create_connection(db_file):
         print(e)
     return None
 
-def get_animals(animal_type):
+def get_animals(animal_type, filters=None):
     title = animal_type.capitalize()
     query = "SELECT animal_name, science_name, images FROM Marine WHERE animal_group = ?"
+    params = [title]
+
+    #adding in filters  maybe try adding <= and >= for the numerical values
+    if filters:
+        if 'life_span' in filters:
+            query += " AND life_span = ?"
+            params.append(filters['life_span'])
+        if 'average_length' in filters:
+            query += " AND average_length = ?"
+            params.append(filters['average_length'])
+        if 'top_speed' in filters:
+            query += " AND top_speed = ?"
+            params.append(filters['top_speed'])
+        if 'mobility' in filters:
+            query += " AND mobility = ?"
+            params.append(filters['mobility'])
+
     con = create_connection(DATABASE)
     cur = con.cursor()
     cur.execute(query, (title,))
@@ -36,6 +53,34 @@ def get_classifications():
     #print(records)
     return records
 
+def get_filters():
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+
+    query_life_span = "SELECT DISTINCT life_span FROM Marine"
+    cur.execute(query_life_span)
+    life_span = [record[0] for record in cur.fetchall()]
+    
+    query_average_length = "SELECT DISTINCT average_length FROM Marine"
+    cur.execute(query_average_length)
+    average_length = [record[0] for record in cur.fetchall()]
+
+    query_top_speed = "SELECT DISTINCT top_speed FROM Marine"
+    cur.execute(query_top_speed)
+    top_speed = [record[0] for record in cur.fetchall()]
+
+    query_mobility = "SELECT DISTINCT mobility FROM Marine"
+    cur.execute(query_mobility)
+    top_speed = [record[0] for record in cur.fetchall()]
+
+
+    con.close()
+    return {
+        'life_span': life_span,
+        'average_length': average_length,
+        'top_speed': top_speed,
+        'mobility': mobility
+        }
 
 @app.route('/')
 def render_home():
