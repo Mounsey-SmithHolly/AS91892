3cd84355c01f7da9d84fefb70e48fd742e57f172
Mounsey-SmithHolly <mounseysmithho@wgc.school.nz>
2025-09-10 22:30:49 +0000

    fixing the code now works

--- main.py
+++ main.py
@@ -15,26 +15,11 @@ def create_connection(db_file):
         print(e)
     return None
 
-def get_animals(animal_type, filters=None):
+def get_animals(animal_type):
     title = animal_type.capitalize()
     query = "SELECT animal_name, science_name, life_span, average_length, top_speed, images FROM Marine WHERE animal_group = ?"
     params = [title]
-
-    #adding in filters  maybe try adding <= and >= for the numerical values
-    if filters:
-        """if 'life_span' in filters:
-            query += " AND life_span = ?"
-            params.append(filters['life_span'])
-        if 'average_length' in filters:
-            query += " AND average_length = ?"
-            params.append(filters['average_length'])
-        if 'top_speed' in filters:
-            query += " AND top_speed = ?"
-            params.append(filters['top_speed'])"""
-        if 'mobility' in filters:
-            query += " AND mobility = ?"
-            params.append(filters['mobility'])
-
+    
     con = create_connection(DATABASE)
     cur = con.cursor()
     cur.execute(query, params)
@@ -49,40 +34,9 @@ def get_classifications():
     cur = con.cursor()
     cur.execute(query)
     records = cur.fetchall()
-    #print(records)
     for i in range(len(records)):
         records[i] = records[i][0]
-    #print(records)
     return records
-
-def get_filters():
-    con = create_connection(DATABASE)
-    cur = con.cursor()
-
-    """query_life_span = "SELECT DISTINCT life_span FROM Marine"
-    cur.execute(query_life_span)
-    life_span = [record[0] for record in cur.fetchall()]
-    
-    query_average_length = "SELECT DISTINCT average_length FROM Marine"
-    cur.execute(query_average_length)
-    average_length = [record[0] for record in cur.fetchall()]
-
-    query_top_speed = "SELECT DISTINCT top_speed FROM Marine"
-    cur.execute(query_top_speed)
-    top_speed = [record[0] for record in cur.fetchall()]"""
-
-    query_mobility = "SELECT DISTINCT mobility FROM Marine"
-    cur.execute(query_mobility)
-    mobility = [record[0] for record in cur.fetchall()]
-
-
-    con.close()
-    return {
-        """'life_span': life_span,
-        'average_length': average_length,
-        'top_speed': top_speed,"""
-        'mobility': mobility
-        }
 
 @app.route('/')
 def render_home():
@@ -90,14 +44,8 @@ def render_home():
 
 @app.route('/main/<classification>')
 def render_animals(classification):
-    filters = {
-        """'life_span': request.args.get('life_span'),
-        'average_length': request.args.get('average_length'),
-        'top_speed': request.args.get('top_speed'),"""
-        'mobility': request.args.get('mobility')
-    }
     title = classification.upper()
-    return render_template('creatures.html', animals=get_animals(classification), title=title, classifications=get_classifications(), filters = get_filters())
+    return render_template('creatures.html', animals=get_animals(classification), title=title, classifications=get_classifications())
 
 #adding in the search function 
 @app.route("/search", methods=['GET','POST'])
@@ -124,7 +72,7 @@ def render_sortpage(title):
         new_order = 'asc'
 
     #sorting query
-    query = "SELECT animal_name, science_name, life_span, average_length, top_speed FROM Marine WHERE animal_group=? ORDER BY " + sort+ " " + order
+    query = "SELECT animal_name, science_name, life_span, average_length, top_speed, images FROM Marine WHERE animal_group=? ORDER BY " + sort+ " " + order
     con = create_connection(DATABASE)
     cur = con.cursor()
 
@@ -136,4 +84,3 @@ def render_sortpage(title):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
